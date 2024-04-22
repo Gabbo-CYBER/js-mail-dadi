b684484077c25a958e1ec50dc25d99a20db75d6b
Gabbo-CYBER <gabry29101998@gmail.com>
2024-04-22 19:27:30 +0200

    first commit

---
+++ dadi.js
@@ -0,0 +1,12 @@
+//giocatore
+let giocatore = Math.floor( Math.random()*6 ); 
+console.log(giocatore)
+document.getElementById("giocatore").innerHTML=giocatore
+
+
+//computer
+let computer = Math.floor( Math.random()*6 ); 
+console.log(computer)
+document.getElementById("computer").innerHTML=computer
+
+
---
+++ email.js
@@ -0,0 +1,14 @@
+const lista = [
+	"Utente-a",
+	"Utente-b",
+	"Utente-c",
+	"Utente-d"
+];
+console.log(lista);
+
+
+
+let utente = prompt('Qual\'è la tua email?');
+console.log(utente);
+
+
